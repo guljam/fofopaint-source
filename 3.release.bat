c15ebfa562387465b6f3803813a2da2bc3d8a63e
guljam <cube4none@gmail.com>
2022-06-20 02:46:26 +0900

    Update 3.release.bat

--- 3.release.bat
+++ 3.release.bat
@@ -9,4 +9,3 @@ git commit --file=versionInfo.txt
 git push
 gh release upload setup D:\fofopaint-source\bin\fofoPaint.exe --clobber
 gh release upload update2 D:\fofopaint-source\bin\fofoPaint.air --clobber
-pause
