ee30359cc13abdabba9d257448a2f582b078bf7d
guljam <cube4none@gmail.com>
2024-05-09 08:32:05 +0900

    25.26

--- _make_air_file.ps1
+++ _make_air_file.ps1
@@ -1,1 +1,1 @@
-E:\AIRSDK_Windows_50.2.2.6\bin\adt.bat -package -storetype pkcs12 -keystore E:\fofopaintKey\secretKey.p12 -storepass 123 bin\fofoPaint.air fofoPaint-app.xml fofoPaint.swf worker.swf resource\icon
+E:\AIRSDK_Windows_50.2.2.6\bin\adt.bat -package -storetype pkcs12 -keystore E:\fofopaintKey\secretKey2.p12 -storepass 123 bin\fofoPaint.air fofoPaint-app.xml fofoPaint.swf worker.swf resource\icon
