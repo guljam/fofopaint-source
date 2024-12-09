b88fa8edaa1945aae6db7b9706769c3103944b1e
guljam <15816034+guljam@users.noreply.github.com>
2024-12-09 11:04:38 +0900

    25.43

--- 2.release.bat
+++ 2.release.bat
@@ -1,12 +1,11 @@
-notepad F:\ssdbackup\2020FlashPaint\versionInfo.txt
+cd F:\ssdbackup\2020FlashPaint
+
+notepad versionInfo.txt
+notepad releasenote.txt
 
 git add .
-git commit --file=F:\ssdbackup\2020FlashPaint\versionInfo.txt
-git push
-
-cd F:\ssdbackup\2020FlashPaint
-git add versionInfo.txt
 git commit --file=versionInfo.txt
 git push
+
 gh release upload setup F:\ssdbackup\fofopaint-source\bin\fofoPaint.exe --clobber
 gh release upload update2 F:\ssdbackup\fofopaint-source\bin\fofoPaint.air --clobber
--- vscode_fofo_auto_release.bat
+++ vscode_fofo_auto_release.bat
@@ -1,17 +1,17 @@
-notepad E:\2020FlashPaint\versionInfo.txt
-notepad E:\2020FlashPaint\releasenote.txt
-notepad E:\2020FlashPaint\README.md
+notepad F:\ssdbackup\2020FlashPaint\versionInfo.txt
+notepad F:\ssdbackup\2020FlashPaint\releasenote.txt
+notepad F:\ssdbackup\2020FlashPaint\README.md
 
 Powershell.exe -noprofile -executionpolicy bypass -file "_make_air_file.ps1"
 
 git add .
-git commit --file=E:\2020FlashPaint\versionInfo.txt
+git commit --file=F:\ssdbackup\2020FlashPaint\versionInfo.txt
 git push
 
-cd E:\2020FlashPaint
+cd F:\ssdbackup\2020FlashPaint
 git add versionInfo.txt
 git add releasenote.txt
 git add README.md
 git commit --file=versionInfo.txt
 git push
-gh release upload update2 E:\fofopaint-source\bin\fofoPaint.air --clobber
+gh release upload update2 F:\ssdbackup\fofopaint-source\bin\fofoPaint.air --clobber
