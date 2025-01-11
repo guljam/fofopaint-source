bdee427fbcc1c059f8d534da6be462f152374948
guljam <15816034+guljam@users.noreply.github.com>
2025-01-12 00:40:09 +0900

    25.61

--- vscode_fofo_auto_release.bat
+++ vscode_fofo_auto_release.bat
@@ -8,10 +8,9 @@ git add .
 git commit --file=F:\2020FlashPaint\versionInfo.txt
 git push
 
-F:\
-git add 2020FlashPaint\versionInfo.txt
-git add 2020FlashPaint\releasenote.txt
-git add 2020FlashPaint\README.md
+cd add F:\2020FlashPaint
+git add .
 git commit --file=F:\2020FlashPaint\versionInfo.txt
 git push
+
 gh release upload update2 E:\fofopaint-source\bin\fofoPaint.air --clobber
