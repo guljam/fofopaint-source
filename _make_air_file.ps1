9a63b9a2073a243d5b73de880f6e755f616c7df6
guljam <15816034+guljam@users.noreply.github.com>
2025-01-30 08:30:03 +0900

    25.73

--- _make_air_file.ps1
+++ _make_air_file.ps1
@@ -1,1 +1,1 @@
-E:\AIRSDK_Windows_51.1.3\AIRSDK_Windows\bin\adt.bat -package -storetype pkcs12 -keystore E:\fofopaintKey\secrekey2.p12 -storepass 123 bin\fofoPaint.air fofoPaint-app.xml fofoPaint.swf worker.swf resource\icon
+E:\AIRSDK_Windows_51.1.3.6\AIRSDK_Windows\bin\adt.bat -package -storetype pkcs12 -keystore E:\fofopaintKey\secrekey2.p12 -storepass 123 bin\fofoPaint.air fofoPaint-app.xml fofoPaint.swf worker.swf resource\icon
